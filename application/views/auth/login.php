c40b3e3dbb60b56d8e5c68b4c087a266381a97d1
Edwin Orellana <edwinorellana@users.noreply.github.com>
2026-05-19 21:45:06 -0600

    add all files

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
 
         <div class="info-card info-card-spaced">
-         
+         <h1>test</h1>
             
             <div id="countdown" data-target="2026-06-10T00:00:00">Cargando cuenta regresiva...</div>
         </div>
